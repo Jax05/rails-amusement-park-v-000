2a333b9369f80d2b682c8e6b901705fff7e79279
Carley Tripp <carley.tripp@outlook.com>
2019-11-02 23:34:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def home
+    render 'layouts/application'
+  end
+
   def current_user
     @user = User.find_by_id(params[:id])
   end
