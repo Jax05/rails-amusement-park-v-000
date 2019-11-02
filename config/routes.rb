7c728ef3a38268ed735f1d7a741d4cf0e9d57019
Carley Tripp <carley.tripp@outlook.com>
2019-11-02 23:40:06 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'application#home'
+  root to: 'application#welcome'
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
 
