e6bfcb6d3277e8ea4b100dd68878bc5584f0f94b
Carley Tripp <carley.tripp@outlook.com>
2019-11-02 22:30:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#new'
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
 
-  resources :users
+  resources :users, :sessions
 end
