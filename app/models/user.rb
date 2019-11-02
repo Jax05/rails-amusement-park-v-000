8ee27d072d12a56f0085e79ce13e70b426bd4666
Carley Tripp <carley.tripp@outlook.com>
2019-11-02 22:18:39 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :name, :happiness, :nausea, :height, :tickets, presence: true
+  has_secure_password
+
+  def mood
+    self.nausea > self.happiness ? "sad" : "happy"
+  end
 end
