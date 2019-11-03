0fc9ae339f2e03cbb4cc1b7316a215a69c7fb235
Carley Tripp <carley.tripp@outlook.com>
2019-11-03 02:01:10 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to users_path(@user)
+      redirect_to user_path(@user)
     else
       render :new
     end
