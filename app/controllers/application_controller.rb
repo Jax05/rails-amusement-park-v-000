4dcf3e21fde6fdf1c2ae4c6adfa76c42ac05de90
Carley Tripp <carley.tripp@outlook.com>
2019-11-03 00:42:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find_by_id(params[:user_id])
+    @current_user ||= User.find_by_id(params[:user_id]) if session[:user_id]
   end
 
   def logged_in?
