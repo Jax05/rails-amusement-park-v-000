7a25b9b66b76b8e5f7af3d74e9317703aa8714c1
Carley Tripp <carley.tripp@outlook.com>
2019-11-03 01:16:50 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_id(params[:id])
 
-    if @user && @user.authenticate(params[:password])
+    if @user && @user.authenticate([:session][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
