1c10a23743fe4c342298e83d52fc3d8a14b34202
Carley Tripp <carley.tripp@outlook.com>
2019-11-03 03:13:48 +0000

    Automatically backed up by Learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,5 +1,17 @@
 class AttractionsController < ApplicationController
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+
   def index
+    @user = User.find(current_user.id)
     @attractions = Attraction.all
   end
 
@@ -7,19 +19,25 @@ class AttractionsController < ApplicationController
     @attraction = Attraction.new
   end
 
-  def create
-
-  end
-
-  def edit
-
+  def show
+    @user = User.find(current_user.id)
+    @attraction = Attraction.find(params[:id])
+    @new_ride = Ride.new
   end
 
   def update
+    @user = User.find(current_user.id)
 
+    if @user.admin
+      @attraction = Attraction.find(params[:id])
+      @attraction.update(attraction_params)
+
+      redirect_to attraction_path(@attraction)
+    end
   end
 
-  def show
 
+  def attraction_params
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
   end
 end
